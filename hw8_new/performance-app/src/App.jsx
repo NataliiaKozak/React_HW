42b5e0de1690e5581cc535da3fbfc9899cd12579
Nataliia Kozak <kozak.svet@gmail.com>
2025-07-03 00:21:16 +0200

    added hw8

---
+++ hw8_new/performance-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListItems from './components/ListItems';
+
+
+function App() {
+
+
+  return (
+    <div>
+      <ListItems />
+    </div>
+  )
+}
+
+export default App
