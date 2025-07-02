ef1d828559b350c8bad4f37a8ac55da4b35c7ded
Nataliia Kozak <kozak.svet@gmail.com>
2025-07-03 01:31:03 +0200

    added hw9

---
+++ hw9/dynamic-form/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DynamicForm from './components/DynamicForm'
+
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center' }}>Dynamic Form</h2>
+      <DynamicForm />
+    </div>
+  )
+}
+
+export default App
