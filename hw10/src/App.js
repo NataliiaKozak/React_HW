a140be13d07f91d9ea146264d96e4bda8ebc0e1f
Nataliia Kozak <kozak.svet@gmail.com>
2025-07-09 01:28:05 +0200

    added hw10 again

--- hw10/src/App.js
+++ hw10/src/App.js
@@ -1,7 +1,12 @@
-cimport './App.css'
+import React from 'react'
+import CatImage from './components/CatImage'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="app">
+      <CatImage />
+    </div>
+  )
 }
 
 export default App
